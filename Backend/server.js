85076eda1813563b541112b4b929412b28ca24f0
walter <musunguwalter57@gmail.com>
2025-12-05 11:54:59 +0300

    Changed the name of the page to "Back-End Web Development"

--- Backend/server.js
+++ Backend/server.js
@@ -3,10 +3,22 @@ const express = require('express');
 const app = express();
 
 app.set("view engine", "ejs");
-app.use
+app.use(express.urlencoded({extended: false}));
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
   res.render('homepage');
 });
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/register', (req, res) => {
+  
+});
+
+
+
 
 app.listen(8383);
