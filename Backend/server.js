b11015f40b0ff9f55f4cdafdc8604b2cb7c6abac
walter <musunguwalter57@gmail.com>
2025-12-16 19:13:48 +0300

    added   user validation codes

--- Backend/server.js
+++ Backend/server.js
@@ -15,6 +15,26 @@ app.get('/login', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
+  const errors = [];
+  if(typeof req.body.username !== 'string') req.body.username = ""
+  if(typeof req.body.password !== 'string') req.body.password = ""
+  
+  req.body.username = req.body.username.trim()// removes the whitespaces for the user, just in case he happens to have left.
+  req.body.password = req.body.password.trim()
+  
+  if(!req.body.username) errors.push('you must provide your username');
+  if(req.body.username && req.body.username.length < 3) errors.push('username must be greater than 3 charactera');
+  if(req.body.username && req.body.username.length > 10) errors.push('username must not exceed 10 charactera');
+  if(req.body.username && req.body.username.match(/^[a-zA-Z0-9]+$/)) errors.push('username can only contain letters and numbers');
+  
+  if(errors.length){
+  return res.render("homepage", {errors})
+  }else{
+  res.send('Registration successful');
+  
+  }
+  
+ 
   
 });
 
